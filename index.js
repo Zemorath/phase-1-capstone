547b4175575f30484a8f30337af4e0bb9c941ca8
Zemorath <trentward10@gmail.com>
2023-07-10 22:02:06 -0500

    Fetched data correctly populating in cards with capability to add book cover

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-let addBook = false;
+
 
 document.addEventListener("DOMContentLoaded", () => {
     let bookFormContainer = document.querySelector(".container");
     let addBtn = document.querySelector("#add-book-button");
     addBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        addBook = !addBook;
-        if (addBook) {
-            bookFormContainer.style.display = "block";
-        } else {
-            bookFormContainer.style.display = "none";
-        }
-
+        event.preventDefault()
         let value = document.getElementById('search').value;
 
         if (value && value.trim().length > 0){
@@ -36,20 +29,40 @@ function fetchBooks(value) {
 };
 
 function renderBooks(books) {
-    console.log(books)
+    console.log(books.docs[0].author_name[0])
     let collection = document.getElementById("book-collection")
     
     let divBook = document.createElement('div');
     divBook.classList.add("card");
 
     let bookName = document.createElement('h2');
-    bookName.innterText = books[0].title;
+    bookName.innerText = books.docs[0].title;
     divBook.appendChild(bookName);
 
     let bookAuthor = document.createElement('h3');
-    bookAuthor.innerText = books[0].author;
+    bookAuthor.innerText = books.docs[0].author_name[0];
     divBook.appendChild(bookAuthor)
 
+    let picButton = document.createElement('button');
+    picButton.setAttribute('class', 'cover')
+    picButton.innerText = "Add cover photo"
+    picButton.addEventListener('click', () => {
+        let url = prompt("Please enter URL")
+        if (url !== null){
+            let bookCover = new Image();
+            bookCover.src = url
+            bookCover.classList.add("book-cover")
+            divBook.appendChild(bookCover)
+            picButton.style.display = "none"
+        } else {
+
+        }
+
+    })
+    divBook.appendChild(picButton)
+
     collection.appendChild(divBook);
+
+    
     
 }
